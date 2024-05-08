5be68b45fb3875ddae3c8e2b06774921341f19ea
dmkhanh <d.m.khanh261203@gmail.com>
2024-05-08 17:03:58 +0700

    Likes-Messages

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -19,9 +19,9 @@ public class AppUser
     public string Country { get; set; }
     public List<Photo> Photos { get; set; } = new();
 
-    //public int GetAge()
-    //{
-    //    return DateOfBirth.CalculateAge();
-    //}
+    public List<UserLike> LikedByUsers { get; set; }
+    public List<UserLike> LikedUsers { get; set; }
+    public List<Message> MessagesSent { get; set; }
+    public List<Message> MessagesReceived { get; set; }
 }
 
